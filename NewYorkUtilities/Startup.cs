87bd36803cb1e10d39d7ccc9a11dfac2745571a2
Sankhya <Sankhyareddy01@gmail.com>
2024-08-19 09:44:55 -0500

    I have planned to build a Web application with an use case of providing a centralized handy application to find any information about New York city. For that I have created a skeleton of project using .Net MVC. I have created Home, Contact and About pages as part the initial commit.

---
+++ NewYorkUtilities/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewYorkUtilities.Startup))]
+namespace NewYorkUtilities
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
